8402a0beb6dfca2747392559a9a4af3f72094d85
Fergus Horrobin <fhorrobin@wifihost-107-21.wireless.utsc.utoronto.ca>
2017-09-27 11:45:05 -0400

    Added material for Tutorial 3

---
+++ triangle.py
@@ -0,0 +1,12 @@
+def triangle_area(base, height):
+    """(number, number) -> float
+    This function takes in two numbers for the base and height of a
+    triangle and returns the area.
+   
+    REQ: base, height >= 0
+    
+    >>> triangle_area(10, 2)
+    10.0
+    """
+    return float(base * height * 0.5)
+
